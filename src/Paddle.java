b12906aa44d7b8aea7121a3c0cd9cbc486437e52
Etienne Lafond <et998726@dal.ca>
2022-12-13 13:47:16 -0400

    Implemented the Paddle() constructor in Paddle.java

--- src/Paddle.java
+++ src/Paddle.java
@@ -9,8 +9,9 @@ public class Paddle extends Rectangle {
     int id; // ID 1 for paddle1, id 2 for paddle2
     int yVelocity;
 
-    Paddle() {
-
+    Paddle(int x, int y, int PADDLE_WIDTH, int PADDLE_HEIGHT, int id) {
+        super(x, y, PADDLE_WIDTH, PADDLE_HEIGHT);
+        this.id = id;
     }
 
     public void keyPressed(KeyEvent e) {
