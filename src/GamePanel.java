4dc10987605b73a02092e8ebfd034cf3d76a5d74
Etienne Lafond <et998726@dal.ca>
2022-12-13 14:51:52 -0400

    Implemented draw() in Score.java

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -58,6 +58,7 @@ public class GamePanel extends JPanel implements Runnable {
         paddle1.draw(g);
         paddle2.draw(g);
         ball.draw(g);
+        score.draw(g);
     }
 
     public void move() {
--- src/Score.java
+++ src/Score.java
@@ -11,10 +11,18 @@ public class Score extends Rectangle {
     int player2;
 
     Score(int GAME_WIDTH, int GAME_HEIGHT) {
-
+        Score.GAME_WIDTH = GAME_WIDTH;
+        Score.GAME_HEIGHT = GAME_HEIGHT;
     }
 
     public void draw(Graphics g) {
+        g.setColor(Color.white);
+        g.setFont(new Font("Consolas", Font.PLAIN, 40));
 
+        g.drawLine(GAME_WIDTH/2, 0, GAME_WIDTH/2, GAME_HEIGHT);
+
+        // Print the name and score of each player in the top-center of the window
+        g.drawString("Player 1 - "+String.valueOf(player1/10)+String.valueOf(player1%10), (GAME_WIDTH/2)-305, 50);
+        g.drawString(String.valueOf(player2/10)+String.valueOf(player2%10)+" - Player 2", (GAME_WIDTH/2)+20, 50);
     }
 }
